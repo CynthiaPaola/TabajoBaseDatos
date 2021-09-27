c0f91e6638a58cc2d0aba1ab1e4affa70da5f354
PGuzman523 <72818073+PGuzman523@users.noreply.github.com>
2021-09-26 22:19:20 -0500

    Cambios y check de base de datos
    documentos y base de tatos terminada

--- ConcursoProgramacion.sql
+++ ConcursoProgramacion.sql
@@ -1,7 +1,7 @@
 Create database ConcursoProgram
 
-USE ConcursoProgram 
-GO
+/*USE ConcursoProgram 
+GO*/
 
 on primary
 (
@@ -37,16 +37,28 @@ go
 
 create table Alumnos(
     
-	numControl int not null,
+	noControl int not null,
 	semestre varchar(2) not null,
 	carrera varchar (100) not null,
-	constraint pk_numControl primary key (numControl)
+	constraint pk_numControl primary key (noControl)
+)
+go
+
+create table BancoProblemas(
+    
+	idBancoProblemas int not null,
+	nombre varchar(100) not null,
+	puntos float not null,
+	tiempMaxEjec time not null,
+	descripcion varchar(500) not null
+	constraint pk_BancoProblemas primary key (idBancoProblemas)
 )
 go
 
 create table ProblemasResueltos(
     
 	idProblemaRes int not null,
+	nomProblemaRes varchar(100) not null,
 	duracion time not null,
 	puntaje float,
 	constraint pk_problemasRes primary key (idProblemaRes)
@@ -85,7 +97,7 @@ create table Ediciones(
 idediciones int not null,
 nombre varchar(30) not null,
 fecharegistro date not null,
-fechaevento date not null,22/JUNIO/2020 19/JUNIO/2020
+fechaevento date not null,
 duracioninicio int not null,
 duracionfinal int not null,
 CONSTRAINT pk_ediciones PRIMARY KEY (idediciones)
@@ -104,9 +116,25 @@ CONSTRAINT pk_equipos PRIMARY KEY (idequipos)
 )
 go
 
-/*Claves externas (FK)*/
+/********************
+Claves unicas (uq)
+********************/
+--Tabla de Usuarios
+alter table Usuarios add constraint Uq_email unique (email)
+go
 
-/*Claves unicas (UQ)*/
+--Tabla de Alumnos
+alter table Alumnos add constraint Uq_noControl unique (noControl)
+go
+
+--Tabla de ProblemasResueltos
+alter table ProblemasResueltos add constraint Uq_Duracion unique (duracion)
+go
+
+--Table de BancoProblemas
+alter table BancoProblemas add constraint Uq_Descripcion unique (descripcion)
+go
+
 ALTER TABLE Docentes ADD CONSTRAINT UQ_Carrera UNIQUE (carrera)
 go
 ALTER TABLE Ediciones ADD CONSTRAINT UQ_Nombre UNIQUE (nombre)
@@ -114,7 +142,39 @@ go
 ALTER TABLE Equipos ADD CONSTRAINT UQ_Categoria UNIQUE (categoria)
 go
 
-/*Restricciones check(CHK)*/
+/**************
+Creacion de Fk 
+**************/
+
+/**********************
+Restricciones de Check
+**********************/
+--Tabla de Usuarios
+alter table Usuarios add constraint chk_Sexo check (sexo in ('F','M'))
+go
+alter table Usuarios add constraint chk_telefono check (telefono like '[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]')
+go
+alter table Usuarios add constraint chk_email check (email like '%@%.%')
+go
+alter table Usuarios add constraint chk_tipo check (tipo in ('Alumnos','Docentes','Asesores'))
+go
+
+--Tabla de Alumnos
+alter table Alumnos add constraint chk_semestre check (semestre like '[0-9][A-Z]')
+go
+alter table Alumnos add constraint chk_noControl check (noControl like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
+go
+
+--Tabla de ProblemasResueltos
+alter table ProblemasResueltos add constraint chk_puntaje check (puntaje<=5)
+go
+
+--Table de BancoProblemas
+/*alter table BancoProblemas add constraint chk_tiempMaxEjec check 
+go*/
+alter table BancoProblemas add constraint chk_puntos check (puntos<=5)
+go
+
 ALTER TABLE Docentes ADD CONSTRAINT CHK_Telefono_Docente CHECK (telefono LIKE '[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]')
 go
 ALTER TABLE Ediciones ADD CONSTRAINT CHK_Fecha_Registro CHECK (fecharegistro BETWEEN YEAR(GETDATE()) AND 9999)
@@ -126,4 +186,25 @@ go
 ALTER TABLE Equipo ADD CONSTRAINT CHK_Categoria CHECK (categoria in ('Facil','Medio','Dificil','Experto'))
 go
 
-/*Restricciones Default (DF)*/
+/*********************
+Restricciones Default
+*********************/
+--Tabla de Usuarios
+alter table Usuarios add constraint Df_password default 'Hola.123' for password
+go
+alter table Usuarios add constraint Df_tipo default 'Usuario' for tipo
+go
+
+--Tabla de Alumnos
+alter table Alumnos add constraint Df_semestre default 1 for semestre
+go
+
+--Tabla de ProblemasResueltos
+alter table ProblemasResueltos add constraint Df_puntos_ProbRes default 1 for puntaje 
+go
+
+--Table de BancoProblemas
+alter table BancoProblemas add constraint Df_puntos_BancoProble default 1 for puntos
+go
+
+
