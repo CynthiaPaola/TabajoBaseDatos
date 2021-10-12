abae3a6cdab63c627d639eee67042e724410ab65
PGuzman523 <72818073+PGuzman523@users.noreply.github.com>
2021-10-12 12:10:34 -0500

    many no trabaja por que se va a manados y no trabaja en el proyecto
    creacion y midificacion de tablas

--- ConcursoProgram.sql
+++ ConcursoProgram.sql
@@ -37,7 +37,6 @@ go
 create table Alumnos(
     
 	noControl int not null,
-	idProblemaRes int not null,
 	idUsuarios int not null,
 	semestre varchar(2) not null,
 	idCarrera int not null,
@@ -60,7 +59,7 @@ create table ProblemasResueltos(
 
 	idProblemaRes int not null,
 	idPropuestos int not null,
-	idEquipos int not null,
+	idEdiciones int not null,
 	duracion time not null,
 	puntaje float,
 	constraint pk_problemasRes primary key (idProblemaRes)
@@ -81,7 +80,6 @@ create table Categorias
 idCategoria int not null,
 nombre varchar(100) not null,
 semestreLimite varchar(20) not null,
-idPropuestos int not null,
 constraint pk_categorias primary key(idCategoria)
 )
 go
@@ -89,32 +87,29 @@ go
 create table Docentes(
 idDocentes int not null,
 idCarrera int not null,
-noControl int not null,
-idUsuarios int not null,
 escolaridad varchar(50) not null,
 especialidad varchar(50) not null,
 cedula varchar(15) not null,
+carrera varchar(100) not null,
 CONSTRAINT pk_docentes PRIMARY KEY (iddocentes)
 )
 go
 
 create table Ediciones(
 idEdiciones int not null,
-idPropuestos  int not null,
-idProblemaRes int not null,
-nombre varchar(100) not null,
+nombre varchar(30) not null,
 fecharegistro date not null,
 fechaevento date not null,
+duracioninicio time not null,
+duracionfinal time not null,
 CONSTRAINT pk_ediciones PRIMARY KEY (idediciones)
 )
 go
 
 create table Equipos(
 idEquipos int not null,
+idEdiciones int not null,
 idCategoria int not null,
-idPropuestos int not null,
-idProblemaRes int not null,
-idDocentes int not null,
 nombre varchar(50) not null,
 nocontrol1 int not null,
 nocontrol2 int not null,
@@ -126,11 +121,9 @@ go
 
 create table Problemaspropuestos(
 idPropuestos int not null,
-idEdiciones int not null,
-idCategoria int not null,
-idBancoProblemas int not null,
+nombre varchar(50) not null,
 globo varchar(10) not null,
-CONSTRAINT pk_edicion PRIMARY KEY (idedicion)
+CONSTRAINT pk_edicion PRIMARY KEY (idPropuestos)
 )
 go
 
@@ -154,7 +147,7 @@ alter table BancoProblemas add constraint Uq_Descripcion unique (descripcion)
 go
 
 --Table de Docentes
-ALTER TABLE Docentes ADD CONSTRAINT UQ_Nocontrol UNIQUE (noControl)
+ALTER TABLE Docentes ADD CONSTRAINT UQ_Carrera UNIQUE (carrera)
 go
 
 --Table de Ediciones
@@ -162,10 +155,10 @@ ALTER TABLE Ediciones ADD CONSTRAINT UQ_Nombre UNIQUE (nombre)
 go
 
 --Table de Equipos
-ALTER TABLE Equipos ADD CONSTRAINT UQ_Nombre UNIQUE (nombre)
+ALTER TABLE Equipos ADD CONSTRAINT UQ_Categoria UNIQUE (categoria)
 go
 
---Table de Problemaspropuestos
+--Table de Edicion
 ALTER TABLE Edicion ADD CONSTRAINT UQ_Globo UNIQUE (globo)
 go
 
@@ -174,8 +167,6 @@ Creacion de Fk
 **************/
 --Tabla de Alumnos
 alter table Alumnos add constraint fk_usuario_alumnos foreign key (idUsuarios) references Usuarios (idUsuarios)
-go
-alter table Alumnos add constraint fk_problemaRes_alumnos foreign key (idProblemaRes) references ProblemasResueltos (idProblemaRes)
 go
 alter table Alumnos add constraint fk_carrera_alumnos foreign key (idCarrera) references Carreras(idCarrera)
 go
@@ -189,37 +180,17 @@ go
 --Table de Docentes
 ALTER TABLE Docentes ADD CONSTRAINT FK_Carreras_Docentes FOREIGN KEY (idCarrera) REFERENCES Carreras(idCarrera)
 go
-ALTER TABLE Docentes ADD CONSTRAINT FK_Usuarios_Docentes FOREIGN KEY (idUsuarios) REFERENCES Usuarios(idUsuarios)
-go
-ALTER TABLE Docentes ADD CONSTRAINT FK_Alumnos_Docentes FOREIGN KEY (noControl) REFERENCES  Alumnos(noControl)
-go
 
 --Table de Ediciones
-ALTER TABLE Ediciones ADD CONSTRAINT FK_Problemaspropuestos_Ediciones FOREIGN KEY (idPropuestos) REFERENCES Problemaspropuestos(idPropuestos)
+ALTER TABLE Ediciones ADD CONSTRAINT FK_Equipos_Ediciones FOREIGN KEY (idEquipos) REFERENCES Equipos(idEquipos)
 go
-ALTER TABLE Ediciones ADD CONSTRAINT FK_ProblemasResueltos_Ediciones FOREIGN KEY (idProblemaRes) REFERENCES ProblemasResueltos(idProblemaRes)
+ALTER TABLE Ediciones ADD CONSTRAINT FK_BancoProblemas_Ediciones FOREIGN KEY (idBancoProblemas) REFERENCES BancoProblemas(idBancoProblemas)
 go
 
 --Table de Equipos
-ALTER TABLE Equipos ADD CONSTRAINT FK_ProblemasResueltos_Equipos FOREIGN KEY (idProblemaRes) REFERENCES ProblemasResueltos(idProblemaRes)
+ALTER TABLE Equipos ADD CONSTRAINT FK_Categorias_Equipos FOREIGN KEY (idCategoria) REFERENCES Categorias(idCategoria)
 go
-ALTER TABLE Equipos ADD CONSTRAINT FK_Problemaspropuestos_Equipos FOREIGN KEY (idProblemaRes) REFERENCES Problemaspropuestos(idPropuestos)
-go
-ALTER TABLE Equipos ADD CONSTRAINT FK_Docentes_Equipos FOREIGN KEY (idDocentes) REFERENCES Docentes(idDocentes) 
-go
-ALTER TABLE Equipos ADD CONSTRAINT FK_ Categorias_Equipos FOREIGN KEY (idCategoria) REFERENCES Categorias(idCategoria)
-go
-
---Table de Problemaspropuestos
-ALTER TABLE Problemaspropuestos ADD CONSTRAINT FK_Categorias_Problemaspropuestos FOREIGN KEY (idCategoria) REFERENCES Categorias(idCategoria)
-go
-ALTER TABLE Problemaspropuestos ADD CONSTRAINT FK_BancoProblemas_Problemaspropuestos FOREIGN KEY (idBancoProblemas) REFERENCES BancoProblemas(idBancoProblemas)
-go
-ALTER TABLE Problemaspropuestos ADD CONSTRAINT FK_Ediciones_Problemaspropuestos FOREIGN KEY (idEdiciones) REFERENCES Ediciones(idEdiciones)
-go
-
---Table Categorias
-alter table Categorias add constraint fk_Problenaspropuestos_Categorias foreign key (idProuestos) references ProblemasPropuestos(idPropuestos)
+ALTER TABLE Equipos ADD CONSTRAINT FK_ProblemasResueltos_Equipos FOREIGN KEY (idProblemaRes) REFERENCES Equipos(idProblemaRes)
 go
 
 /**********************
@@ -256,17 +227,19 @@ ALTER TABLE Docentes ADD CONSTRAINT CHK_Telefono_Docente CHECK (telefono LIKE '[
 go
 
 --Table de Ediciones
-ALTER TABLE Ediciones ADD CONSTRAINT CHK_Fecha_Registro CHECK (fecharegistro BETWEEN YEAR(GETDATE()) AND 9999)
+ALTER TABLE Ediciones ADD CONSTRAINT CHK_Fecha_Registro CHECK (fecharegistro LIKE '[0-9][0-9][0-9][0-9]/[0-9][0-9]/[0-9][0-9]')
 go
 ALTER TABLE Ediciones ADD CONSTRAINT CHK_Fecha_Evento CHECK(fecharegistro<fechaevento)
 go
 
 --Table de Equipo
-ALTER TABLE Equipo ADD CONSTRAINT CHK_Nombre CHECK (nombre LIKE '%%%%%%%%%%%%%%%%%%%%%%')
+ALTER TABLE Equipo ADD CONSTRAINT CHK_Integrantes CHECK (integrantes<5)
+go
+ALTER TABLE Equipo ADD CONSTRAINT CHK_Categoria CHECK (categoria in ('Facil','Medio','Dificil','Experto'))
 go
 
---Table de Problemaspropuestos
-ALTER TABLE Problemaspropuestos ADD CONSTRAINT CHK_Globo CHECK (globo in ('Verde','Amarillo','Rojo','Negro'))
+--Table de Edicion
+ALTER TABLE Edicion ADD CONSTRAINT CHK_Globo CHECK (globo in ('Verde','Amarillo','Rojo','Negro'))
 go
 
 /*********************
@@ -295,15 +268,15 @@ ALTER TABLE Docentes ADD CONSTRAINT DF_cedula_Docente default 'XXX-XXX-XXXX' for
 go
 
 --Table de Ediciones
-ALTER TABLE Ediciones ADD CONSTRAINT DF_fecharegistro default getdate() for fecharegistro
-go
+/*ALTER TABLE Ediciones ADD CONSTRAINT DF*/
 
 --Table de Equipos
 ALTER TABLE Equipos ADD CONSTRAINT DF_puntos_Equipos default 1 for puntos
 go
 
---Table de ProbblemasPropuestos
-ALTER TABLE Problemaspropuestos ADD CONSTRAINT DF_globo default 'Blanco' for globo
+--Table de Edicion
+ALTER TABLE Edicion ADD CONSTRAINT DF_globo_Edicion default 'Blanco' for globo
 go
 
 
+drop database ConcursoProgram
